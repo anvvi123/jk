39cbba6e3bfdca88c4deba9c53836463ee8b33ac
anvvi123 <78200009+anvvi123@users.noreply.github.com>
2021-02-08 19:00:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+ctx.beginPath();
+ctx.strokeStyle="grey";
+ctx.lineWidth=4;
+ctx.rect(150,143,430,200);
+ctx.stroke();
+ctx.beginPath();
+ctx.strokeStyle="blue";
+ctx.lineWidth=5;
+ctx.rect(250,210,40,0,2 * Math.PI);
+ctx.stroke();
+ctx.beginPath();
+ctx.strokeStyle="black";
+ctx.lineWidth=5;
+ctx.rect(350,210,40,0,2 * Math.PI);
+ctx.stroke();
